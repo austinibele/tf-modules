1c3f00add9a8b23e2637160bf01e8d8f5c804ab2
austinibele <austin.ibele@kungfu.ai>
2024-01-14 09:08:05 -0500

    Remove lifecycle rule

--- alb/main.tf
+++ alb/main.tf
@@ -40,8 +40,4 @@ resource "aws_lb_target_group" "alb_tg" {
     protocol    = var.protocol 
     target_type = var.target_type 
     vpc_id      = var.vpc_id 
-
-    lifecycle {
-        create_before_destroy = true
-    }
 }
