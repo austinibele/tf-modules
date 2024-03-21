9caf468b1df217978ea6e053abacefe1df2ee4c3
austinibele <austin.ibele@kungfu.ai>
2024-03-21 06:59:07 -0400

    Add dns_name and zone_id as outputs

--- alb/output.tf
+++ alb/output.tf
@@ -17,3 +17,13 @@ output "https_listener" {
     value = (var.create_alb && var.enable_https) ? aws_lb_listener.https[0]: null
     description = "The https listner info"
 }
+
+output "dns_name" {
+    value = var.create_alb ? aws_lb.alb[0].dns_name : null
+    description = "The DNS name of the load balancer"
+}
+
+output "zone_id" {
+    value = var.create_alb ? aws_lb.alb[0].zone_id : null
+    description = "The zone id of the load balancer"
+}
