1c3ce344f03e2e823dc6a67bdb1dfd4fdaea8478
austinibele <austin.ibele@kungfu.ai>
2024-03-24 12:54:08 -0400

    remove 0 indexing

--- alb/output.tf
+++ alb/output.tf
@@ -1,24 +1,24 @@
 output "tg" {
-    value = var.create_target_group ? aws_lb_target_group.alb_tg[0] : null
+    value = var.create_target_group ? aws_lb_target_group.alb_tg : null
     description = "The target group info"
 }
 
 output "lb" {
-    value = var.create_alb ? aws_lb.alb[0] : null
+    value = var.create_alb ? aws_lb.alb : null
     description = "The load balancer info"
 }
 
 output "listener" {
-    value = var.create_alb ? aws_lb_listener.listener[0] : null
+    value = var.create_alb ? aws_lb_listener.listener : null
     description = "The listener info"
 }
 
 output "dns_name" {
-    value = var.create_alb ? aws_lb.alb[0].dns_name : null
+    value = var.create_alb ? aws_lb.alb.dns_name : null
     description = "The DNS name of the load balancer"
 }
 
 output "zone_id" {
-    value = var.create_alb ? aws_lb.alb[0].zone_id : null
+    value = var.create_alb ? aws_lb.alb.zone_id : null
     description = "The zone id of the load balancer"
 }
